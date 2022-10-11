83d9f5f1c4089ed8b14b7d6dbe92fe338a21a180
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-10-11 20:32:25 -0300

    Initial commit

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function recordInfo() {
-  var usuario = document.getElementById("usuario").value;
-  var senha = document.getElementById("senha").value;
-  localStorage.setItem(user, usuario);
-  localStorage.setItem(password, senha);
-  alert("Dados salvos com sucesso!");
-
-}
